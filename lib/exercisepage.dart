50706e1c72f58d5a44ae28bc1ce519742f72bc02
Shirish Tamrakar <shirishtmkr987@gmail.com>
2026-01-08 18:43:57 +0545

    api/llm integration

---
+++ lib/exercisepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ExerciseScreen extends StatefulWidget {
+  const ExerciseScreen({super.key});
+
+  @override
+  State<ExerciseScreen> createState() => _ExerciseScreenState();
+}
+
+class _ExerciseScreenState extends State<ExerciseScreen> {
+  
+
+  static const _bgColor = Color(0xFF0A2852);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      
+                        
+                    );
+  }
+  }
